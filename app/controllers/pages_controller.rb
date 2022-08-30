2b3b1dad501ec2bd006003c4d69e07e6ad32a6e8
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-08-30 16:14:22 +1000

    styling for forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  before_action :fetch_user  #This will run in all controllers, but can be controlled with 'except' and 'only
+  
   def home
+      if @current_user.present?
+        redirect_to hooks_path
+      else
+        redirect_to login_path
+      end
   end
 end
