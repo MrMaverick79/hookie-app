8d59e9a374e57db192ce8b76ab368b1064015b36
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-08-31 14:57:15 +1000

    Added switches for private and NSFW

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,13 @@
 module LinksHelper
+
+    def show_hook_on_select link
+
+        if link.hooks.first == nil
+            return
+        else
+            link.hooks.first.title
+        end
+
+    end
+
 end
