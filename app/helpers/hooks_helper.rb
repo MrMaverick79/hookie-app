a87e1c3c1a11451e6ba2d7be4f5542d100dcdbb6
Brendan Tuckerman <brendan.tuckerman@gmail.com>
2022-09-01 07:04:38 +1000

    Styling & pre-icon push

--- app/helpers/hooks_helper.rb
+++ app/helpers/hooks_helper.rb
@@ -1,5 +1,29 @@
 module HooksHelper
   
+    def show_icon link #take a link and shows the icon if available
+     
+       if l.icon?
+                    
+                  
+        <% if l.url[0..3] == 'http' %>
+           <% if (l.url =~ URI::regexp) %>
+                  <%= image_tag l.icon %>
+            <% else %>
+                <%= image_tag 'dot.png'%>
+            <% end  %>
+        <% else %>
+            <% url =  'https://' + l.icon %>
+            <% begin open(url).status[1] == 'OK'  %>
+            <% rescue  %>
+                <%= image_tag 'dot.png'%>
+                  
+            <% else %>
+                <%= image_tag url %>
+            <% end  %>
+        <% end  %>
+    <% else %>
+        <%= image_tag 'dot.png'%>
+    <% end  %>
     
     def site_icon_link site
         if site[0..3]!= 'http'
